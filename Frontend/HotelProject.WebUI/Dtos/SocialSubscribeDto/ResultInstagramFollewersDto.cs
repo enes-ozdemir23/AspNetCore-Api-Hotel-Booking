f112516f1c70e48159de4dcaa002eb3d4d95ed20
enes-ozdemir23 <149109601+enes-ozdemir23@users.noreply.github.com>
2026-03-14 03:21:18 +0300

    #Rapid api ile sosyal medya hesap bilgilerinin alınması

---
+++ Frontend/HotelProject.WebUI/Dtos/SocialSubscribeDto/ResultInstagramFollewersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelProject.WebUI.Dtos.SocialSubscribeDto
+{
+    public class ResultInstagramFollewersDto
+    {
+        public int followers { get; set; }
+        public int following { get; set; }
+    }
+}
--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using HotelProject.WebUI.Dtos.SocialSubscribeDto;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace HotelProject.WebUI.ViewComponents.Dashboard
 {
     public class _DashboardSubscribeCountPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofileinfo/murattycedag"),
+                Headers =
+    {
+        { "x-rapidapi-key", "94772ac3eamsh4b856e2a2f33290p1f991bjsn61695af6ea34" },
+        { "x-rapidapi-host", "instagram-profile1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ResultInstagramFollewersDto resultInstagram = JsonConvert.DeserializeObject<ResultInstagramFollewersDto>(body);
+                ViewBag.itakipci = resultInstagram.followers;
+                ViewBag.itakip = resultInstagram.following;
+                return View();
+            }
+
+
         }
     }
 }

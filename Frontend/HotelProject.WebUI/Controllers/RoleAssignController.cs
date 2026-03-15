7a71a6f2160bc0fde3eebb1bd6758ef01c8b5291
enes-ozdemir23 <149109601+enes-ozdemir23@users.noreply.github.com>
2026-03-16 00:27:44 +0300

    #Rol atama sayfası tasarlandı ve Rol atama işlemi yapıldı

--- Frontend/HotelProject.WebUI/Controllers/RoleAssignController.cs
+++ Frontend/HotelProject.WebUI/Controllers/RoleAssignController.cs
@@ -1,12 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Models.Role;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class RoleAssignController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleAssignController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var values=_userManager.Users.ToList();
+            return View(values);
         }
+
+        public async Task<IActionResult> AssingRole(int id)
+        {
+            var user=_userManager.Users.FirstOrDefault(x => x.Id == id);
+            TempData["Userid"]=user.Id;
+            var roles=_roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            List<RoleAssignViewModel> roleAssignViewModels= new List<RoleAssignViewModel>();
+            foreach(var item in roles)
+            {
+                RoleAssignViewModel model=new RoleAssignViewModel();
+                model.RoleID=item.Id;
+                model.RoleName=item.Name;
+                model.RoleExist=userRoles.Contains(item.Name);
+                roleAssignViewModels.Add(model);
+            }
+            return View(roleAssignViewModels);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssingRole(List<RoleAssignViewModel> roleAssignViewModel)
+        {
+            var userid = (int)TempData["userid"];
+            var user=_userManager.Users.FirstOrDefault(x=>x.Id == userid);
+            foreach(var item in roleAssignViewModel)
+            {
+                if (item.RoleExist)
+                {
+                    await _userManager.AddToRoleAsync(user,item.RoleName);
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user,item.RoleName);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
     }
 }
